a7746648514afacef7af5589bfcb566610fa50eb
Mustafa Nomair <66224841+mustafa-nom@users.noreply.github.com>
2025-05-26 12:12:00 -0700

    updated ViewController code
    the loaner laptop I had that I did the code on is gone so I added the code I forgot to originally import.

--- SEADeni/ViewController.swift
+++ SEADeni/ViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aqiLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+
+    let networkManager = NetworkManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchAndDisplayAQI()
+    }
+
+    func fetchAndDisplayAQI() {
+        networkManager.fetchAQI { [weak self] aqi in
+            DispatchQueue.main.async {
+                guard let self = self, let aqi = aqi else {
+                    self?.aqiLabel.text = "Error"
+                    self?.emojiLabel.text = "❓"
+                    return
+                }
+
+                self.aqiLabel.text = "AQI: \(aqi)"
+                self.emojiLabel.text = self.emoji(for: aqi)
+            }
+        }
+    }
+
+    //the bitmoji pictures were on my loaner laptop & the images were lost so here are emojis in place for them for the time being --> its the same effect
+    func emoji(for aqi: Int) -> String {
+        switch aqi {
+        case ...50:
+            return "😊"
+        case 51...100:
+            return "😐"
+        default:
+            return "😷"
+        }
     }
 
 
